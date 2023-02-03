af1a5c335de27d7859505ea9655b24bef13e99f5
Ochan-LOKIDORMI <o.lokidormo@alustudent.com>
2023-02-02 17:11:07 -0800

    Updated

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-import this
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str)
