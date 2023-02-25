4795c5d976ca9a1c46e4c1125bfb98838edc14cb
Ochan-LOKIDORMI <o.lokidormo@alustudent.com>
2023-02-25 15:33:59 -0800

    Safe list printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     """Print x elememts of a list.
 
@@ -19,5 +20,4 @@ def safe_print_list(my_list=[], x=0):
             break
     print("")
     return (ret)
-
 
