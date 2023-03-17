ce607e652feedc60f5df9239661497af71153568
Ochan-LOKIDORMI <o.lokidormo@alustudent.com>
2023-03-17 10:19:45 -0700

    0-9

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -41,17 +41,17 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """This return the area of the Rectangle."""
+        """Return the area of the Rectangle."""
         return (self.__width * self.__height)
 
     def perimeter(self):
-        """This return the perimeter of the Rectangle."""
+        """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
-        """This return the printable representation of the Rectangle.
+        """Return the printable representation of the Rectangle.
         Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
@@ -65,11 +65,11 @@ class Rectangle:
         return ("".join(rect))
 
     def __repr__(self):
-        """This return the string representation of the Rectangle."""
+        """Return the string representation of the Rectangle."""
         rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
         return (rect)
 
     def __del__(self):
-        """This print a message for every deletion of a Rectangle."""
+        """Print a message for every deletion of a Rectangle."""
         print("Bye rectangle...")
