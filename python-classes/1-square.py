588edc3befef85c34193e0ab3d2cb214a152eba2
Ochan-LOKIDORMI <o.lokidormo@alustudent.com>
2023-03-17 05:34:39 -0700

    1-6

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This script defines a class Square."""
+
+
+class Square:
+    """This represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+
