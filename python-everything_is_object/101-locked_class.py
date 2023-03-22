dc2a122c0c020a6913798e3935852035f651d2f3
Ochan-LOKIDORMI <o.lokidormo@alustudent.com>
2023-03-22 09:46:05 -0700

    Low memory cost

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""def a locked class"""
+"""Defines a locked class."""
 
 
-class lockedclass:
+class LockedClass:
     """
-    Avoid the user from giving new lockedclass attributes
-    for anything ut only for the one called 'first_name'.
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
     """
 
     __slots__ = ["first_name"]
