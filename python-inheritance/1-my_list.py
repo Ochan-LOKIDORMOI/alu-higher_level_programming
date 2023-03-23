0875eff7d5202292d6b5c8d9216e8a10cee7ac69
Ochan-LOKIDORMI <o.lokidormo@alustudent.com>
2023-03-23 16:22:10 -0700

    My List

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bib/python3
+"""Module inherited from the list class"""
+
+
+class myList(list):
+    """A class inherited from list"""
+    def print_sorted(self):
+        """This print a sorted list"""
+        print(sorted(self))
