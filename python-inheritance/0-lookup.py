cdd6b1652d19794fefc2c15034185bb36feb2274
Ochan-LOKIDORMI <o.lokidormo@alustudent.com>
2023-03-23 16:12:40 -0700

    Look up

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""This defines an object lookup function."""
+"""Defines an object lookup function."""
 
 
 def lookup(obj)
-    """Return a list of an object's available attributes"""
+    """Return a list of an object"""
     return dir(obj)
