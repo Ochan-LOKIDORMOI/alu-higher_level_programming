f49035f38df429d9327c21aebe97dba1ed4255d8
Ochan-LOKIDORMI <o.lokidormo@alustudent.com>
2023-03-23 17:13:09 -0700

    Improve module

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -10,4 +10,4 @@ class BaseGeometry:
         """
         This raises an exception with the sms "area() is not made".
         """
-        raise Exception("area() is not made")
+        raise Exception("area() is not implemented")
