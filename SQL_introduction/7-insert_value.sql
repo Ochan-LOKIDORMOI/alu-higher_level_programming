7fca3305c0f7667ee63da090f839f51d2b0b063f
Ochan-LOKIDORMI <o.lokidormo@alustudent.com>
2023-03-30 08:17:58 -0700

    First add

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- The script that insert a new row in a table.
-INSERT INTO first_table (id, name) VALUES (89, 'Holberton school'); 
+INSERT INTO first_table (id, name) VALUES (89, 'Holberton School'); 
