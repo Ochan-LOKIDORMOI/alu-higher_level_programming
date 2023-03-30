8767c1f11dff2adddb5805fc14ec03d2267cebad
Ochan-LOKIDORMI <o.lokidormo@alustudent.com>
2023-03-30 16:29:35 -0700

    Go to UTF8

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+-- The script that covert database
+-- The script that convert table
+-- The script that convert field
+
+ALTER DATABASE hbtn_0c_0 CHARACTER SET unf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
