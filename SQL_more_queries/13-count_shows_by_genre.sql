c8f9906e4cbafd7a63a016770a9828e15db14393
Ochan-LOKIDORMI <o.lokidormo@alustudent.com>
2023-03-31 09:54:25 -0700

    Number of shows by genre

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Import the database dump from the number of shows by genre.
+
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres, tv_show_genres
+WHERE tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
