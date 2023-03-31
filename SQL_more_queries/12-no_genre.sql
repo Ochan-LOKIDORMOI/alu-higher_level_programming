0594124c1725d97b0fcaf81ef504ebf8d2fcf12e
Ochan-LOKIDORMI <o.lokidormo@alustudent.com>
2023-03-31 09:43:45 -0700

    No genre

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- Impor the database dump from no genre.
+
+SELECT tv_show.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id
