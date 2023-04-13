1bdfca627eed6e48187da84b3a2bdfe098a71c95
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-04-13 14:55:33 -0700

    TDD

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Doc"""
+""""Doc"""
 
 
 import unittest
@@ -8,24 +8,24 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInt(unittest.TestCase):
-    """Test Cases"""
+    """" Test Cases"""
 
     def test_simple_case(self):
-        """Doc"""
+        """"Doc"""
         self.assertEqual(max_integer([1, 2, 3]), 3)
 
     def test_negative_case(self):
-        """Doc"""
-        self.assertEqual(max_integer([-1, -2, -3]), 3)
-    
+        """"Doc"""
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
+
     def test_empty_case(self):
-        """Doc"""
+        """"Doc"""
         self.assertEqual(max_integer([]), None)
 
     def test_max_middle_case(self):
-        """Doc"""
-        self.assertEqual(max_integer([4, 7, 2, 8, 3]), 8)
+        """"Doc"""
+        self.assertEqual(max_integer([7, 2, 8, 3, 4]), 8)
 
     def test_one_element_case(self):
-        """Doc"""
+        """"Doc"""
         self.assertEqual(max_integer([5]), 5)
