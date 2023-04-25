b7ea1dd54bd75f2f2cc65ee2ba9b31b6c7f842c9
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-04-25 16:16:34 -0700

    Delete states

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    This is a script thst deletes all State objects 
+    This is a script thst deletes all State objects
     with a name containing the letter a from the database hbtn_0e_6_usa
 """
 
