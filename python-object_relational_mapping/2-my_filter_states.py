7831a789a02022dff03b55fbfd8043ababbab27b
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-04-25 14:56:35 -0700

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-     a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa
-     where name matches the argument.
+     This is a script that takes in an argument and displays all
+     values in the states table of hbtn_0e_0_usa
+     where name matches the argument
 """
 
 import MySQLdb
@@ -19,7 +20,6 @@ if __name__ == '__main__':
                          user=username,
                          passwd=password,
                          db=db_name)
-    
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(state_name))
     states = cursor.fetchall()
