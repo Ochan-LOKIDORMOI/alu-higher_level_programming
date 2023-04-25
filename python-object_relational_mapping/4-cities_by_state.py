3adbc864fd1bfb9eb858777ca4e4960679c472a9
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-04-25 15:27:53 -0700

    Cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -4,22 +4,30 @@
     This is a script that lists all cities from the database hbtn_0e_4_usa
 """
 
-import MySQLdb
 import sys
+import MySQLdb
+
 
 if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
 
-    db = MySQLdb.connect(host='localhost',
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=db_name)
-    
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM cities ORDER BY id ASC")
-    cities = cursor.fetchall()
-    for city in cities:
-        print(city)
+
+    sql = """SELECT c.id, c.name, s.name
+          FROM states s, cities c
+          WHERE c.state_id = s.id
+          ORDER BY c.id ASC"""
+
+    cursor.execute(sql)
+
+    data = cursor.fetchall()
+
+    for row in data:
+        print(row)
+
+    cursor.close()
+    db.close()
