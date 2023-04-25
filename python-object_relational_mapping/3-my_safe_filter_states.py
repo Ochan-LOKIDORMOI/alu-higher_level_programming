b6a47e2977f304672997b73a1a49aa74f5d9ee9f
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-04-25 15:19:54 -0700

    SQL Injection...

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-    This is a script that takes in arguments and 
+    This is a script that takes in arguments and
     displays all values in the states table of hbtn_0e_0_usa
     where name matches the argument.
     But this time, write one that is safe from MySQL injections!
