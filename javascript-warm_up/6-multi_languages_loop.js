96bff25c45fa75416e42af55c5362ae0a2154c1d
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-06-10 04:16:44 -0700

    Loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -2,4 +2,4 @@
 
 const lines = ['JavaScript is amazing', 'Python is cool', 'C is fun'];
 
-for (const line of lines.reverse()) console.log(line);g
+for (const line of lines.reverse()) console.log(line);
