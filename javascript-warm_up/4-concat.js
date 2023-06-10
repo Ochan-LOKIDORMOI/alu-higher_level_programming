e168ecc165a4f9a56071b8d7298a5045b071ef1f
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-06-10 03:53:33 -0700

    Create a sentence

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 
-const argument1 = process.argv[2];
-const argument2 = process.argv[3];
+const {argv } = process;
 
-console.log(argpument1 + 'is ' + argument2);
+console.log(argv[2], 'is', argv[3]);
