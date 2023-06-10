f081ced59a0d51fa423459f6b82b92269b14ec1c
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-06-10 03:33:45 -0700

    Arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-const numArgs = argv.lenth;
+const numArgs = process.argv.length - 2;
 
 if (numArgs === 0) {
-console.log('No argument');
+  console.log('No argument');
 } else if (numArgs === 1) {
-console.log('Argument found');
+  console.log('Argument found');
 } else {
-console.log('Arguments found');
+  console.log('Arguments found');
 }
