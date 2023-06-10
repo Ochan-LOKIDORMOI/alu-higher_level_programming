a4cdf92fcfa6a5dbf9e9c2b2e9c24a628d8e9d91
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-06-10 05:49:21 -0700

    Object

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-const myObject = {
-  { type: 'object', value: 12 }
-  { type: 'object', value: 89 }
-};
-console.log(myObject);
-/*
-YOUR CODE HERE
-*/
-myObject.value = 89;
-console.log(myObject);
+const args = process.argv.slice(2).map(Number);
+
+if (args.length === 0 || args.length === 1) {
+  console.log(0);
+} else {
+  const updatedArgs = args.map((value) => (value === 12 ? 89 : value));
+  const sortedArgs = updatedArgs.sort((a, b) => b - a);
+  console.log(sortedArgs[1]);
+}
