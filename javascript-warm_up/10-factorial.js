4072366c89bc8eba04b43c353b191f2ceed43db5
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-06-10 05:26:44 -0700

    Factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
- function factorial(n) {
+function factorial(n) {
   if (isNaN(n)) {
     return 1;
   }
@@ -12,8 +12,4 @@
   return n * factorial(n - 1);
 }
 
-const argument = process.argv[2];
-const num = parseInt(argument);
-
-const result = factorial(num);
-console.log(result);
+const argument = process
