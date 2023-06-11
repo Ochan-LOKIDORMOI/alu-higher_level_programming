0fb7cdb6c524f7243ffa1f8ff612f62aa1db6016
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-06-11 09:19:02 -0700

    Rectangle 3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,22 +1,11 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || !Number.isInteger(w) || h <= 0 || !Number.isInteger(h)) {
-      return {};
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+      }
+     }
     }
-    this.width = w;
-    this.height = h;
-  }
-
-  print() {
-    if (Object.keys(this).length === 0) {
-      console.log("Empty rectangle");
-      return;
-    }
-
-    for (let i = 0; i < this.height; i++) {
-      console.log("X".repeat(this.width));
-    }
-  }
-}
+module.exports = Rectangle;
