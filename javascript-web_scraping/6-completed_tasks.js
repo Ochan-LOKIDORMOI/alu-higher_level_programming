722bdd280a6f2375507aab4a0cabc30d8ff6ddaf
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-06-14 03:29:28 -0700

    How many completed?

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,34 @@
 #!/usr/bin/node
 
 const request = require('request');
+
+function getCompletedTasks (data, userId) {
+  let count = 0;
+  data
+    .filter((element) => element.userId === userId)
+    .forEach((task) => {
+      if (task.completed) {
+        count++;
+      }
+    });
+  return count;
+}
+
 const url = process.argv[2];
 
-request(url, function (err, response, body) {
+const results = {};
+request.get(url, (err, res) => {
   if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
+    throw err;
+  }
+  const body = res.body;
+  const data = JSON.parse(body);
+  data.forEach((element) => {
+    if (!(element.userId in results)) {
+      if (getCompletedTasks(data, element.userId) > 0) {
+        results[element.userId] = getCompletedTasks(data, element.userId);
       }
     }
-    console.log(completed);
-  } else {
-    console.log('An error occurred. Status code: ' + response.statusCode);
-  }
+  });
+  console.log(results);
 });
